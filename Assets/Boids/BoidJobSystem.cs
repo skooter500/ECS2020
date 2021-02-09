7f588854879c261257eb6c4cde9aaf44e5fafc2a
Bryan Duggan <skooter500@gmail.com>
2021-02-09 11:35:52 +0000

    bla

--- Assets/Boids/BoidJobSystem.cs
+++ Assets/Boids/BoidJobSystem.cs
@@ -246,37 +246,15 @@ namespace ew
 
         protected override void OnUpdate()
         {
-            /*
             BoidBootstrap bootstrap = this.bootstrap;
-            // Copy to local variables. Required for the lambdas            
-            float dT = Time.DeltaTime;
 
-            float wanderWeight = bootstrap.wanderWeight;
-            float limitUpAndDown = bootstrap.limitUpAndDown;
-            float bondDamping = bootstrap.bondDamping;
+            ComponentTypeHandle<Wander> wTHandle = GetComponentTypeHandle<Wander>();
+            ComponentTypeHandle<Boid> bTHandle = GetComponentTypeHandle<Boid>();
+            ComponentTypeHandle<Translation> ttTHandle = GetComponentTypeHandle<Translation>();
+            ComponentTypeHandle<Rotation> rTHandle = GetComponentTypeHandle<Rotation>();
 
-            float neighbourDistance = bootstrap.neighbourDistance;
-            int cellSize = bootstrap.cellSize;
-            bool usePartitioning = bootstrap.usePartitioning;
-            bool threedcells = bootstrap.threedcells;
-            int gridSize = bootstrap.gridSize;
-            int maxNeighbours = this.maxNeighbours;
-            int spineLength = bootstrap.spineLength;
-            int spineOffset = bootstrap.spineLength / 2;
-            float seperationWeight = bootstrap.seperationWeight;
-            float alignmentWeight = bootstrap.alignmentWeight;
-            float cohesionWeight = bootstrap.cohesionWeight;
-            float seekWeight = bootstrap.seekWeight;
-            float fleeWeight = bootstrap.fleeWeight;
-            float fleeDistance = bootstrap.fleeDistance;
-
-            NativeArray<int> neighbours = this.neighbours;            
-            NativeArray<Vector3> positions = this.positions;
-            NativeArray<Quaternion> rotations = this.rotations;
-            NativeArray<float> speeds = this.speeds;
-            NativeMultiHashMap<int, int> cells = this.cells;
-            */
-
+            float deltaTime = Time.DeltaTime;
+            
             Unity.Mathematics.Random ran = new Unity.Mathematics.Random((uint)UnityEngine.Random.Range(1, 100000));
 
             // Copy entities to the native arrays             
@@ -290,235 +268,21 @@ namespace ew
             })
             .ScheduleParallel(this.Dependency);
 
-            /*
-            cells.Clear();
-            var partitionJob = new PartitionSpaceJob()
+            Dependency = JobHandle.CombineDependencies(Dependency, copyToNativeHandle);
+            
+            var wanderJob = new WanderJob()
             {
-                positions = this.positions,
-                //cells = this.cells,
-                threedcells = bootstrap.threedcells,
-                cellSize = bootstrap.cellSize,
-                gridSize = bootstrap.gridSize
+                wanderTypeHandle = wTHandle,
+                translationTypeHandle = ttTHandle,
+                rotationTypeHandle = rTHandle,
+                boidTypeHandle = bTHandle,
+                dT = deltaTime,
+                weight = bootstrap.wanderWeight
             };
 
-            var partitionHandle = partitionJob.Schedule(bootstrap.numBoids, 50, copyToNativeHandle);
-            
-            var countHandle = Entities
-                .WithNativeDisableParallelForRestriction(positions)
-                .WithNativeDisableParallelForRestriction(cells)
-                .WithNativeDisableParallelForRestriction(neighbours)
-                .ForEach((Boid b) =>
-            {
-                int neighbourStartIndex = maxNeighbours * b.boidId;
-                
-                int neighbourCount = 0;
-                if (usePartitioning)
-                {
-                    int surroundingCellCount = (int)Mathf.Ceil(neighbourDistance / cellSize);
-
-                    // Are we looking above and below? 
-                    int sliceSurrounding = threedcells ? surroundingCellCount : 0;
-                    for (int slice = -sliceSurrounding; slice <= sliceSurrounding; slice++)
-                    {
-                        for (int row = -surroundingCellCount; row <= surroundingCellCount; row++)
-                        {
-                            for (int col = -surroundingCellCount; col <= surroundingCellCount; col++)
-                            {
-                                Vector3 pos = positions[b.boidId] + new Vector3(col * cellSize, slice * cellSize, row * cellSize);
-                                int cell = PartitionSpaceJob.PositionToCell(pos, cellSize, gridSize);
-
-                                NativeMultiHashMapIterator<int> iterator;
-                                int boidId;
-                                if (cells.TryGetFirstValue(cell, out boidId, out iterator))
-                                {
-                                    do
-                                    {
-                                        if (boidId != b.boidId)
-                                        {
-                                            if (Vector3.Distance(positions[b.boidId], positions[boidId]) < neighbourDistance)
-                                            {
-                                                neighbours[neighbourStartIndex + neighbourCount] = boidId;
-                                                neighbourCount++;
-                                                if (neighbourCount == maxNeighbours)
-                                                {
-                                                    b.taggedCount = neighbourCount;
-                                                    return;
-                                                }
-                                            }
-                                        }
-                                    } while (cells.TryGetNextValue(out boidId, ref iterator));
-                                }
-                            }
-                        }
-
-                    }
-                }
-                else
-                {
-                    for (int i = 0; i < positions.Length; i++)
-                    {
-                        if (i != b.boidId)
-                        {
-                            if (Vector3.Distance(positions[b.boidId], positions[i]) < neighbourDistance)
-                            {
-                                neighbours[neighbourStartIndex + neighbourCount] = i;
-                                neighbourCount++;
-                                if (neighbourCount == maxNeighbours)
-                                {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-                b.taggedCount = neighbourCount;                
-            }
-            )
-            .ScheduleParallel(partitionHandle);
-
-            var seperationHandle = Entities
-                .WithNativeDisableParallelForRestriction(positions)
-                .WithNativeDisableParallelForRestriction(neighbours)
-                .ForEach((ref Boid b, ref Seperation s) =>
-            {
-                Vector3 force = Vector3.zero;
-                int neighbourStartIndex = maxNeighbours * b.boidId;
-                int mySpineId = b.boidId * (spineLength + 1);
-                Vector3 myPosition = positions[mySpineId + spineOffset];
-                for (int i = 0; i < b.taggedCount; i++)
-                {
-                    int neighbourId = neighbours[neighbourStartIndex + i];
-                    if (neighbourId == b.boidId)
-                    {
-                        continue;
-                    }
-                    int neighbourSpineId = (neighbourId * (spineLength + 1)) + spineOffset;
-                    //Vector3 toNeighbour = positions[b.boidId] - positions[neighbourId];
-                    Vector3 toNeighbour = myPosition - positions[neighbourSpineId];
-                    float mag = toNeighbour.magnitude;
-                    //force += (Vector3.Normalize(toNeighbour) / mag);
-
-
-                    if (mag > 0) // Need this check otherwise this behaviour can return NAN
-                    {
-                        force += (Vector3.Normalize(toNeighbour) / mag);
-                    }
-                    else
-                    {
-                        // same position, so generate a random force
-                        Vector3 f = ran.NextFloat3Direction();
-                        force += f * b.maxForce;
-                    }
-                }
-                s.force = force * seperationWeight;
-            })
-            .ScheduleParallel(countHandle);
-
-            
-            var cohesionHandle = Entities
-                .WithNativeDisableParallelForRestriction(positions)
-                .WithNativeDisableParallelForRestriction(neighbours)
-                .ForEach((ref Boid b, ref Cohesion c) =>
-            {
-                Vector3 force = Vector3.zero;
-                Vector3 centerOfMass = Vector3.zero;
-                int neighbourStartIndex = maxNeighbours * b.boidId;
-                for (int i = 0; i < b.taggedCount; i++)
-                {
-                    int neighbourId = neighbours[neighbourStartIndex + i];
-                    centerOfMass += positions[neighbourId];
-                }
-                if (b.taggedCount > 0)
-                {
-                    centerOfMass /= b.taggedCount;
-                    // Generate a seek force
-                    Vector3 toTarget = centerOfMass - positions[b.boidId];
-                    Vector3 desired = toTarget.normalized * b.maxSpeed;
-                    force = (desired - b.velocity).normalized;
-                }
-
-                c.force = force * cohesionWeight;
-            })
-            .ScheduleParallel(seperationHandle);   
-            */
-
-            /*var alHandle = Entities
-            .WithNativeDisableParallelForRestriction(positions)
-            .WithNativeDisableParallelForRestriction(rotations)
-            .WithNativeDisableParallelForRestriction(speeds)
-            .ForEach((ref Boid b, ref Alignment a) =>
-            {
-                
-                Vector3 desired = Vector3.zero;
-                Vector3 force = Vector3.zero;
-                int neighbourStartIndex = maxNeighbours * b.boidId;
-                for (int i = 0; i < b.taggedCount; i++)
-                {
-                    int neighbourId = neighbours[neighbourStartIndex + i];
-                    desired += rotations[neighbourId] * Vector3.forward;
-                }
-
-                if (b.taggedCount > 0)
-                {
-                    desired /= b.taggedCount;
-                    force = desired - (rotations[b.boidId] * Vector3.forward);
-                }
-
-                a.force = force * alignmentWeight;
-            })
-            .ScheduleParallel(cohesionHandle);
-                        
-            var wjHandle = Entities
-            .WithNativeDisableParallelForRestriction(positions)
-            .WithNativeDisableParallelForRestriction(rotations)
-            .WithNativeDisableParallelForRestriction(speeds)            
-            .ForEach((ref Boid b, ref Wander w, ref Translation p, ref Rotation r) =>
-            {
-                Vector3 disp = w.jitter * ran.NextFloat3Direction() * dT;
-                w.target += disp;
-                w.target.Normalize();
-                w.target *= w.radius;
-
-                Vector3 localTarget = (Vector3.forward * w.distance) + w.target;
-
-                Quaternion q = r.Value;
-                Vector3 pos = p.Value;
-                Vector3 worldTarget = (q * localTarget) + pos;
-                w.force = (worldTarget - pos) * wanderWeight;
-            })
-            .ScheduleParallel(alHandle);            
-            // Integrate the forces
-            
-            var boidHandle = Entities
-            .WithNativeDisableParallelForRestriction(positions)
-            .WithNativeDisableParallelForRestriction(rotations)
-            .WithNativeDisableParallelForRestriction(speeds)
-            .ForEach((ref Boid b, ref Seperation s, ref Alignment a, ref Cohesion c, ref Wander w, ref Constrain con) =>
-            {
-                float banking = 0.1f;
-                b.force = AccululateForces(ref b, ref s, ref a, ref c, ref w, ref con) * b.weight;
-
-                b.force = Vector3.ClampMagnitude(b.force, b.maxForce);
-                Vector3 newAcceleration = (b.force * b.weight) / b.mass;
-                newAcceleration.y *= limitUpAndDown;
-                b.acceleration = Vector3.Lerp(b.acceleration, newAcceleration, dT);
-                b.velocity += b.acceleration * dT;
-                b.velocity = Vector3.ClampMagnitude(b.velocity, b.maxSpeed);
-                //b.velocity.y *= 0.8f;
-                float speed = b.velocity.magnitude;
-                speeds[b.boidId] = speed;
-                if (speed > 0)
-                {
-                    Vector3 tempUp = Vector3.Lerp(b.up, (Vector3.up) + (b.acceleration * banking), dT * 3.0f);
-                    rotations[b.boidId] = Quaternion.LookRotation(b.velocity, tempUp);
-                    b.up = rotations[b.boidId] * Vector3.up;
-
-                    positions[b.boidId] += b.velocity * dT;
-                    b.velocity *= (1.0f - (bondDamping * dT));
-                }
-            })
-            .ScheduleParallel(wjHandle);
-            */
+            var wanderJobHandle = wanderJob.ScheduleParallel(wanderQuery, 1, Dependency);
+            Dependency = JobHandle.CombineDependencies(Dependency, wanderJobHandle);
+                    
             var cfJobHandle = Entities
             .WithNativeDisableParallelForRestriction(positions)
             .WithNativeDisableParallelForRestriction(rotations)
@@ -528,10 +292,42 @@ namespace ew
                 r.Value = rotations[b.boidId];
             })
             .ScheduleParallel(boidHandle);
-            Dependency = cfJobHandle;
+            Dependency = JobHandle.CombineDependencies(Dependency, cfJobHandle);            
             return;
         }
     }
+
+    [BurstCompile]
+        struct CopyTransformsToJob : IJobProcessComponentData<Position, Rotation, Boid>
+        {
+            [NativeDisableParallelForRestriction]
+            public NativeArray<Vector3> positions;
+            [NativeDisableParallelForRestriction]
+            public NativeArray<Quaternion> rotations;
+
+            public void Execute(ref Position p, ref Rotation r, ref Boid b)
+            {
+                positions[b.boidId] = p.Value;
+                rotations[b.boidId] = r.Value;
+            }
+
+        }
+
+        [BurstCompile]
+        struct CopyTransformsFromJob : IJobEntityBatch
+        {
+            [NativeDisableParallelForRestriction]
+            public NativeArray<Vector3> positions;
+
+            [NativeDisableParallelForRestriction]
+            public NativeArray<Quaternion> rotations;
+
+            public void Execute()
+            {
+                p.Value = positions[b.boidId];
+                r.Value = rotations[b.boidId];
+            }
+        }
 
     struct WanderJob : IJobEntityBatch
     {
